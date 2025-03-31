ba6f5f4a723f5c249de463d0ec17a8e0053e465c
nielskoster2000 <niels-koster@outlook.com>
2025-03-31 19:14:01 +0200

    Minor styling improvements

--- resources/views/components/housingcard.blade.php
+++ resources/views/components/housingcard.blade.php
@@ -1,6 +1,6 @@
-<div class=" rounded-lg shadow-md">
-    <img src="{{ $house['images'][0]['url'] }}" alt="House Image" class="rounded-t-lg">
-    <div class="px-8 py-5 flex flex-col">
+<div class="rounded-lg shadow-md flex flex-col">
+    <img src="{{ $house['image'] }}" class="rounded-t-lg h-48 object-cover">
+    <div class="px-4 py-4 flex flex-col">
         <span class="font-semibold"> {{ $house['street'] }} </span>
         <span class=" text-stone-400 mb-4"> {{ $house['city'] }} </span>
         <div class="flex justify-between">
@@ -8,7 +8,7 @@
                 <span class="font-semibold">€ {{ $house['price'] }} </span>
                 <span class=" text-stone-600"> / maand</span>
             </div>
-            <a href="{{ $house['housing_url'] }}" target="_blank" class=" text-blue-700 font-semibold">Meer zien</a>
+            <span class=" text-blue-700 font-semibold">Meer zien</span>
         </div>
     </div>
 </div>
