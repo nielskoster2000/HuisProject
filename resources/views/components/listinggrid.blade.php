031710c0f008508674d6b5a931ab7a40ac0bdaff
nielskoster2000 <niels-koster@outlook.com>
2025-04-01 21:24:39 +0200

    Add CTA card at 3rd index in housing card grid

--- resources/views/components/listinggrid.blade.php
+++ resources/views/components/listinggrid.blade.php
@@ -1,5 +1,9 @@
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
     @foreach ($houses as $house)
+        @if ($loop->index == 3)
+            <x-ctacard></x-ctacard>
+        @else
         <x-housingcard :house="$house"></x-housingcard>
+        @endif
     @endforeach
 </div>
