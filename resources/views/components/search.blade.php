8f19934ed4004e67146e15e34638ead42681e333
nielskoster2000 <niels-koster@outlook.com>
2025-04-01 22:11:54 +0200

    Restored placeholder search

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,4 +1,4 @@
 <div class="flex flex-col gap-2">
     <span class="font-semibold">Zoeken</span>
-    <input id="search" class="border border-stone-200 rounded-sm outline-0 px-2 py-1">
+    <input id="search" class="border border-stone-200 rounded-sm outline-0 px-2 py-1" placeholder="Amsterdam">
 </div>
