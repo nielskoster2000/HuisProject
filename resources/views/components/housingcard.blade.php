8c73d6830f24910b92bf6abf50f64d46d94303a8
nielskoster2000 <niels-koster@outlook.com>
2025-04-02 20:21:08 +0200

    Add img fallback to housing cards

--- resources/views/components/housingcard.blade.php
+++ resources/views/components/housingcard.blade.php
@@ -1,5 +1,5 @@
 <div class="rounded-lg shadow-md flex flex-col h-[21rem]">
-    <img src="{{ $house->image }}" class="rounded-t-lg h-54 object-cover">
+    <img src="{{ $house->image ?? asset('storage/fallback.jpg') }}" class="rounded-t-lg h-54 object-cover">
     <div class="px-4 py-4 flex flex-col">
         <span class="font-semibold"> {{ $house->street }} </span>
         <span class=" text-stone-400 mb-4"> {{ $house->city }} </span>
