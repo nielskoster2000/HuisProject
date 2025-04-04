3a1179c0f12621cae0e076437309bb9fdd720575
nielskoster2000 <niels-koster@outlook.com>
2025-04-04 09:58:25 +0200

    Implement CTA card props

--- app/View/Components/ctacard.php
+++ app/View/Components/ctacard.php
@@ -8,12 +8,20 @@ use Illuminate\View\Component;
 
 class ctacard extends Component
 {
+    public ?string $title;
+    public ?string $emailPlaceholder;
+    public ?string $cityPlaceholder;
+    public ?array $options;
+
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(?string $title = null, ?string $emailPlaceholder = null, ?string $cityPlaceholder = null, ?array $options = null)
     {
-        //
+        $this->title = $title ?? 'Vind jouw ideale huurwoning met onze slimme zoekservice';
+        $this->emailPlaceholder = $emailPlaceholder ?? 'Bijv. voorbeeld@mail.nl';
+        $this->cityPlaceholder = $cityPlaceholder ?? 'Amsterdam';
+        $this->options = $options ?? ['Direct', 'Indirect'];
     }
 
     /**
@@ -21,6 +29,11 @@ class ctacard extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.ctacard');
+        return view('components.ctacard', [
+            'title' => $this->title,
+            'emailPlaceholder' => $this->emailPlaceholder,
+            'cityPlaceholder' => $this->cityPlaceholder,
+            'options' => $this->options,
+        ]);
     }
 }
--- resources/views/components/ctacard.blade.php
+++ resources/views/components/ctacard.blade.php
@@ -1,11 +1,13 @@
 <div class="rounded-lg shadow-md flex flex-col bg-slate-900 text-sm md:text-md xl:text-lg h-[21rem]">
     <div class="flex flex-col md:gap-5 gap-3 my-auto px-8">
-        <h1 class="text-white font-semibold text-center text-lg xl:text-2xl">Vind jouw ideale huurwoning met onze slimme zoekservice</h1>
-        <input class="border border-stone-200 rounded-lg outline-0 md:p-2 p-1 bg-white" placeholder="Bijv. voorbeeld@mail.nl">
+        <h1 class="text-white font-semibold text-center text-lg xl:text-2xl">{{ $title ?? 'Vind jouw ideale huurwoning met onze slimme zoekservice' }}</h1>
+        <input class="border border-stone-200 rounded-lg outline-0 md:p-2 p-1 bg-white" placeholder="{{ $emailPlaceholder ?? 'Bijv. voorbeeld@mail.nl' }}">
         <div class="flex md:flex-row flex-col space-between md:gap-5 gap-2">
-            <input class="md:w-1/2 border border-stone-200 rounded-lg outline-0 md:p-2 p-1  bg-white" placeholder="Amsterdam">
+            <input class="md:w-1/2 border border-stone-200 rounded-lg outline-0 md:p-2 p-1  bg-white" placeholder="{{ $cityPlaceholder ?? 'Amsterdam' }}">
             <select class="md:w-1/2 border border-stone-200 rounded-lg md:p-2 p-1 bg-white">
-                <option>Direct</option>
+                @foreach ($options ?? ['Direct', 'Indirect'] as $option)
+                    <option value="$option">{{ $option }}</option>
+                @endforeach
             </select>
         </div>
         <button class="bg-blue-700 text-white md:p-2 p-1 rounded-lg shadow-2xl shadow-blue-700 font-semibold">Zoekopdracht aanmaken</button>
